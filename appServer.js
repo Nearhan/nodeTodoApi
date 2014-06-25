ffcc4f0171903fa384e652298c346c6a7e22155c
Coleman McFarland <coleman.mcfarland@gmail.com>
2014-06-24 22:42:29 -0400

    Removed routes.js

--- appServer.js
+++ appServer.js
@@ -1,9 +1,19 @@
 var express = require('express');
-var util = require('util');
-var fs = require('fs');
-
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 var router = require('./routes')();
 
 app.use('/', router);
-app.listen(8000);
+server.listen(8000);
+
+
+io.sockets.on('connection', function(socket) {
+
+	socket.on('data', function(msg) { 
+
+	    console.log(msg);
+	});
+
+	socket.emit('ready', {message: 'Ready.'});
+});
